319bc4d40c3a185dd92538b9d396a9fc89c9f549
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-03-29 23:41:18 -0400

    added to thought route

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+// Requires express package for routing
+const router = require('express').Router();
+
+// Grabs the functions from the thoughtController file in the controllers folder
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction).delete(removeReaction);
+
+module.exports = router;
